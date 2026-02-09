f522f3b451b95b5f82b0318137f912b73eba2996
홍길동 <hyunwoo8@kocea.or.kr>
2026-02-09 17:14:41 +0900

    fix: vite build with index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,19 @@ import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+export default defineConfig({  
+
+  root: ".",          // ⭐ 프로젝트 루트를 기준으로
+  build: {
+    outDir: "dist",   // 결과물은 dist
+    emptyOutDir: true
+  },
+});
+  // plugins: [
+  //   build(),
+  //   devServer({
+  //     adapter,
+  //     entry: 'src/index.tsx'
+  //   })
+  // ]
+  //})
